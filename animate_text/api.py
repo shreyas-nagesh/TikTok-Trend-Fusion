afc9f4ea3f20d4655682060502dcd3cbc13456e0
Srivarshan Selvaraj <srivarshan02@gmail.com>
2024-07-06 17:58:10 -0700

    refactor: Move get_accel_device() function to seperate module

--- animate_text/api.py
+++ animate_text/api.py
@@ -1,27 +1,5 @@
 from diffusers import DiffusionPipeline
-import torch
-
-
-# Define a function to get the acceleration device to be used by the pipeline
-def get_accel_device() -> str:
-    """
-    Returns the acceleration device to be used by the pipeline.
-
-    Args:
-        None
-
-    Returns:
-        str: Acceleration device to be used by the pipeline
-
-    Examples:
-        >>> get_accel_device() -> "cuda"
-    """
-    if torch.cuda.is_available():
-        return "cuda"  # Use CUDA if available
-    elif torch.backends.mps.is_available():
-        return "mps"  # Use MPS if available
-    else:
-        return "cpu"  # Fall back to CPU if no other options are available
+from utils import get_accel_device
 
 
 # Define a function to generate an image from a given description
